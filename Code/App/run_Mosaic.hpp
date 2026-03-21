07cf74456dd0d4a0b2811a7ebf28c05fa7d6d58f
Simon Sprouse <simonhsprouse@gmail.com>
2026-03-21 14:40:11 -0500

    abstracted routine logic away from main.cpp into new app folder

---
+++ Code/App/run_Mosaic.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+
+namespace app { 
+    void runMosaic(std::string image_path, std::string save_dir);
+}
--- Code/main.cpp
+++ Code/main.cpp
@@ -9,8 +9,7 @@
 #include "../Test/Utils/Graphics/graphicsTest.hpp"
 #include "../Test/Routines/Mosaic/MosaicTest.hpp"
 
-#include "Data/Image/io.hpp"
-#include "Routines/Mosaic/Mosaic.hpp"
+#include "App/run_Mosaic.hpp"
 
 
 
@@ -24,36 +23,11 @@ int main() {
     string file_system_image_path = "../Images/flower.jpg";
     string file_system_save_dir = "../Results/";
 
-    mosaic_gen::Parameters params;
-    params.resize_factor = 0.5;
-    params.blur_kernel_size = 3;
-    params.blur_sigma = 1.4;
-    params.canny_threshold_1 = 50;
-    params.canny_threshold_2 = 100;
-    params.max_segment_angle_rad = 100 * M_PI / 180.0; // TODO why is this in rad
-    params.min_segment_length = 20;
-    params.segment_angle_window = 10;
-    params.tile_size = 10;
-    params.number_of_rings = 3;
-    params.initial_step = 1.5 * params.tile_size;
-    params.step_size = 0.25 * params.tile_size;
-    params.min_intersection_distance = 0.25 * params.tile_size;
-    params.max_frontiers = 7;
-    params.flood_fill_neighbor_points = 4;
-    params.distance_from_center = 1.5 * params.tile_size;
-    params.random_background_points = 50000;
-    params.canny_resize_factor = 0.4;
-    // params.tiles_per_frame = 20;
-    // params.jitter_map.insert({4, 0});
-    // params.jitter_map.insert({8, 1});
-    // params.jitter_map.insert({12, 10});
-
-    int squeeb = 42;
 
 
 
-    mosaic_gen::test::MosaicTest my_mosaic_test(params, file_system_image_path, file_system_save_dir);
-    my_mosaic_test.runAllTests();
+    // mosaic_gen::test::MosaicTest my_mosaic_test(params, file_system_image_path, file_system_save_dir);
+    // my_mosaic_test.runAllTests();
 
 
 
@@ -66,20 +40,9 @@ int main() {
     auto start = std::chrono::high_resolution_clock::now();
 
 
+    app::runMosaic(file_system_image_path, file_system_save_dir);
 
-
-    Image img = image::io::loadImageFileSystem(file_system_image_path);
-    cout << "Loaded image from: " << file_system_image_path << endl;
-    cout << "Original Dimensions: " << img.size() << endl;
-
-    Mosaic my_mosaic(params);
-    my_mosaic.loadExistingImage(img);
-    my_mosaic.runAll();
-
-    cout << "Results Dimensions: " << my_mosaic.getCanvas().size() << endl;
-    image::io::saveImageFileSystem(my_mosaic.getCanvas(), "../Results/result.jpg");
-
-
+   
 
 
     auto end = std::chrono::high_resolution_clock::now();
