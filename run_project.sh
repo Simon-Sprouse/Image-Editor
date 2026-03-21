eb4cf5241b2a31af3937dd66e4286c8ecf3afca7
Simon Sprouse <simonhsprouse@gmail.com>
2026-03-21 12:50:50 -0500

    project compiles and runs with test image

---
+++ run_project.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# This script is used to run cmake in the build directory.
+# TODO make script env invariant
+# TODO make versions of opencv + other libs dynamic install + check
+# TODO add d flag to destroy Build folder and c for make clean
+# TODO remove images from Results folder somehow
+
+echo "Running cmake in the Build Directory"
+
+
+### --- Parse Args --- ###
+
+# Mac getopt cannot handle longoptions correctly TODO: make this system invariant
+GETOPT=$(brew --prefix gnu-getopt)/bin/getopt
+
+ARGS=$("$GETOPT" --options cr --longoptions clean_build,run -- "$@")
+eval set -- "$ARGS"
+
+bool_clean_build=false
+bool_run_build=true
+
+while true; do
+    case "$1" in 
+        -c | --clean_build ) bool_clean_build=true; shift ;;
+        -r | --run ) bool_run_build=true; shift ;;
+        -- ) break;;
+    esac
+done
+
+### --- Build Project --- ###
+
+if [ "$bool_clean_build" = true ] ; then
+    echo "Removing Build directory "
+    rm -rf Build
+fi
+
+mkdir -p Build
+
+cd Build
+
+cmake ../Code
+
+make
+
+if [ "$bool_run_build" = true ] ; then  
+    ./mosaic_tiler
+fi
